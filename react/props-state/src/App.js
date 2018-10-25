7afdfb42e4751050e9b16c199f55ec68e0b97a27
Adrián Méndez <adrian@bincat.me>
2018-10-25 08:40:28 -0500

    ejemplo en clases

--- react/props-state/src/App.js
+++ react/props-state/src/App.js
@@ -5,7 +5,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <Message message={"Hello"} />
+        {/* <Message message={"Hello"} /> */}
+        <Message />
       </div>
     );
   }
--- react/props-state/src/Message.js
+++ react/props-state/src/Message.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 
 export default class Message extends Component {
   state ={
-    name: 'Adrián'
+    name: 'Adrián',
+    show: false
+  }
+
+  constructor(props){
+    super(props);
+
+    this.message = this.props.message || "Hello Default";
   }
 
   render() {
     return (
-      <h1>{this.props.message}, {this.state.name}</h1>
+      <h1>{this.message}, 
+      {
+        this.state.show && <h1>{this.state.name}</h1>
+      }</h1>
     );
   }
 
   componentWillMount(){
     setTimeout(() => {
-      this.setState({name: 'Julio'});
-    },5000);
+      this.setState({show: true});
+    },2000);
   }
 }
